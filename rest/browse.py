3d251d63afc58132ef2e55a11d341be710bd43e3
Kalimaha <GeoBricks2012>
2014-07-24 16:02:13 +0200

    Service to list source products

--- rest/browse.py
+++ rest/browse.py
@@ -1,4 +1,10 @@
 from flask import Blueprint
+from flask import Flask
+from flask import Response
+from flask.ext.cors import cross_origin
+from utils import config as c
+from ftplib import FTP
+import json
 
 browse = Blueprint('browse', __name__)
 
@@ -6,6 +12,13 @@ browse = Blueprint('browse', __name__)
 def index():
     return 'Welcome to the Browse module!'
 
-@browse.route('/<user_name>')
-def name(user_name):
-    return 'Welcome to the Browse module, ' + user_name + '!'
+@browse.route('/<source_name>')
+@cross_origin(origins='*')
+def list_products(source_name):
+    config = c.Config(source_name.upper())
+    ftp = FTP(config.json['source']['ftp']['base_url'])
+    ftp.login()
+    ftp.cwd(config.json['source']['ftp']['data_dir'])
+    l = ftp.nlst()
+    ftp.quit()
+    return Response(json.dumps(l), content_type = 'application/json; charset=utf-8')
---
+++ utils/config.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+
+class Config:
+
+    def __init__(self, filename):
+
+        """
+        Initialize the class with the filename of a JSON stored in the config directory.
+        @param filename: Name of the JSON file stored in the config lib to be read
+        """
+
+        self.filename = filename
+        path = os.path.join('../config/')
+
+        json_data = open(path + self.filename + '.json').read()
+        self.config = json.loads(json_data)
+        self.json = json.loads(json_data)
+
+    def __init__(self, filename, path_to_file=""):
+
+        """
+        Initialize the class with the filename of a JSON stored in the config directory.
+        @param filename: Name of the JSON file stored in the config lib to be read
+        """
+        dir = os.path.dirname(os.path.dirname(__file__))
+        self.filename = filename
+        file_path = dir + '/config/' + path_to_file
+        json_data = open(file_path + "/" + self.filename + '.json').read()
+        self.config = json.loads(json_data)
+        self.json = json.loads(json_data)
+
+    def get(self, property):
+
+        """
+        Read a property of the JSON file stored in the config directory and set in the constructor.
+        @param property: Name of the JSON key to access the property
+        @return: Value corresponding to the given key
+        """
+        return self.config[property]
