8e92f15c45cabbb2c8744538a42d668de97111b5
Kalimaha <GeoBricks2012>
2014-07-24 16:34:52 +0200

    list source's products

--- rest/browse.py
+++ rest/browse.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
-from flask import Flask
 from flask import Response
 from flask.ext.cors import cross_origin
 from utils import config as c
@@ -15,11 +14,18 @@ def index():
 @browse.route('/<source_name>')
 @cross_origin(origins='*')
 def list_products(source_name):
-    config = c.Config(source_name.upper())
-    if config.json['source']['type'] == 'FTP':
-        ftp = FTP(config.json['source']['ftp']['base_url'])
-        ftp.login()
-        ftp.cwd(config.json['source']['ftp']['data_dir'])
-        l = ftp.nlst()
-        ftp.quit()
-        return Response(json.dumps(l), content_type = 'application/json; charset=utf-8')
+    try:
+        config = c.Config(source_name.upper())
+        if config.json['source']['type'] == 'FTP':
+            ftp = FTP(config.json['source']['ftp']['base_url'])
+            ftp.login()
+            ftp.cwd(config.json['source']['ftp']['data_dir'])
+            l = ftp.nlst()
+            ftp.quit()
+            return Response(json.dumps(l), content_type = 'application/json; charset=utf-8')
+        else:
+            m = 'Source type "' + config.json['source']['type'] + '" is not currently supported.'
+            return Response(json.dumps(m), content_type = 'application/json; charset=utf-8')
+    except:
+        m = 'Source type "' + config.json['source']['type'] + '" is not currently supported.'
+        return Response(json.dumps(m), content_type = 'application/json; charset=utf-8')
--- rest/main.py
+++ rest/main.py
@@ -1,4 +1,5 @@
-from rest import app, config
+from rest import app
+from rest import config
 
 if __name__ == '__main__':
     app.run(port=config.PORT, debug=config.DEBUG)
