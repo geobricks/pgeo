1ddc3c02d328aaa1b1133fbc278324a74e0795e4
Simone Murzilli <simone.murzilli@fao.org>
2014-07-29 13:19:57 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,11 @@
+# To be used by Flask: DEVELOPMENT ONLY
 DEBUG = True
+
+# Flask port: DEVELOPMENT ONLY
 PORT = 5005
+
+# Default folder root to store layers. Each data provider configuration file specifies the path AFTER this folder.
 TARGET_ROOT = '/home/Desktop/GIS'
+
+# Each folder cntains one layer only. This is the default file name for such layers.
 DEFAULT_LAYER_NAME = 'layer.geotiff'
