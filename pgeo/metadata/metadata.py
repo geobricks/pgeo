a1602cfe1b440c6cf3177e21062b7debb42ac8f9
guidofenix <guido.barbaglia@fao.org>
2014-08-05 15:36:17 +0200

    Merge utilis

--- pgeo/metadata/metadata.py
+++ pgeo/metadata/metadata.py
@@ -1,24 +1,10 @@
 from pgeo.config.settings import read_template
-from copy import deepcopy
+from pgeo.utils.json import dict_merge
 
 
 def merge_layer_metadata(template_name, data):
-    template = read_template('core')
-    out = dict(template, **data)
-    test = dict_merge(template, data)
-    return test
-
-
-def dict_merge(a, b):
-    '''
-    Source: https://www.xormedia.com/recursively-merge-dictionaries-in-python/
-    '''
-    if not isinstance(b, dict):
-        return b
-    result = deepcopy(a)
-    for k, v in b.iteritems():
-        if k in result and isinstance(result[k], dict):
-            result[k] = dict_merge(result[k], v)
-        else:
-            result[k] = deepcopy(v)
-    return result
+    core_template = read_template('core')
+    template = read_template(template_name)
+    out = dict_merge(core_template, data)
+    out = dict_merge(out, template)
+    return out
