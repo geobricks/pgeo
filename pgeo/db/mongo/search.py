4e304106bf209a51ff454399bcd0f749b315a577
guidofenix <guido.barbaglia@fao.org>
2014-08-06 16:20:13 +0200

    Docs

--- pgeo/db/mongo/search.py
+++ pgeo/db/mongo/search.py
@@ -9,10 +9,22 @@ doc = settings['db']['metadata']['document']['layer']
 
 
 def find_layer_by_id(layer_id):
+    """
+        Find a layer's metadata by Mongo uid
+        @param layer_id: MongoDB uid
+        @return: Metadata document in JSON format.
+    """
     return client[db][doc].find_one({'_id': ObjectId(layer_id)})
 
 
 def find_layers_by_product(product, dekad, agg_type):
+    """
+        Find layers according to user's selections.
+        @param product: Product ID, e.g. 'MOD13Q1'
+        @param dekad: Dekad, e.g. 08-1 for August 1st dekad
+        @param agg_type: Aggregation type: none, avg or da
+        @return: Array of metadata document in JSON format.
+    """
     q = {}
     conditions = []
     if product is not None:
