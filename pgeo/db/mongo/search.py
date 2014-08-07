20040b74e9f469a259dfa7a6f84ad497b2cdce63
guidofenix <guido.barbaglia@fao.org>
2014-08-07 13:50:16 +0200

    Documentation for MongoSearch

--- pgeo/db/mongo/search.py
+++ pgeo/db/mongo/search.py
@@ -1,16 +1,16 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from pgeo.config.settings import settings
-
-
-client = MongoClient(settings['db']['metadata']['connection'])
-db = settings['db']['metadata']['database']
-doc = settings['db']['metadata']['document']['layer']
 
 
 class MongoSearch():
 
     def __init__(self, connection, db_name, table_name):
+        """
+
+        @param connection: Connection string for MongoDB
+        @param db_name: Schema name for MongoDB
+        @param table_name: Document name for MongoDB
+        """
         self.connection = connection
         self.db_name = db_name
         self.table_name = table_name
@@ -18,19 +18,19 @@ class MongoSearch():
 
     def find_layer_by_id(self, layer_id):
         """
-            Find a layer's metadata by Mongo uid
-            @param layer_id: MongoDB uid
-            @return: Metadata document in JSON format.
+        Find a layer's metadata by Mongo uid
+        @param layer_id: MongoDB uid
+        @return: Metadata document in JSON format.
         """
-        return client[db][doc].find_one({'_id': ObjectId(layer_id)})
+        return self.client[self.db][self.doc].find_one({'_id': ObjectId(layer_id)})
 
     def find_layers_by_product(self, product, dekad, agg_type):
         """
-            Find layers according to user's selections.
-            @param product: Product ID, e.g. 'MOD13Q1'
-            @param dekad: Dekad, e.g. 08-1 for August 1st dekad
-            @param agg_type: Aggregation type: none, avg or da
-            @return: Array of metadata document in JSON format.
+        Find layers according to user's selections.
+        @param product: Product ID, e.g. 'MOD13Q1'
+        @param dekad: Dekad, e.g. 08-1 for August 1st dekad
+        @param agg_type: Aggregation type: none, avg or da
+        @return: Array of metadata document in JSON format.
         """
         q = {}
         conditions = []
@@ -41,4 +41,4 @@ class MongoSearch():
         if agg_type is not None:
             conditions.append({'meStatisticalProcessing.seDatasource.seDataCompilation.aggregationProcessing': agg_type})
         q['$and'] = conditions
-        return client[self.db_name][self.table_name].find(q)
+        return self.client[self.db_name][self.table_name].find(q)
