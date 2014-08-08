66c78e5a18a1cfcf4696c120b3185b343cea03a7
vortex <simone.murzilli@fao.org>
2014-08-08 14:44:18 +0200

    geocoding update

--- pgeo/gis/geocoding.py
+++ pgeo/gis/geocoding.py
@@ -4,14 +4,30 @@ from pgeo.utils import log
 
 geolocator = Nominatim()
 
+
 def get_location(place):
     """
     @param place: place to search i.e. "FAO, roma"
-    @param string:
-    @return: location obj containing the location.address and  location.latitude location.longitude
-             retrun None if place not found
+    @type: string:
+    @return: location obj containing the location.raw, location.address and location.latitude location.longitude
+             return None if place not found
     """
     try:
         return geolocator.geocode(place)
+    except Exception, e:
+        raise PGeoException(e, status_code=400)
+
+
+def get_reverse(lat, lon):
+    """
+    @param lat: place to search i.e. "52.509669"
+    @type: float
+    @param lon: place to search i.e. "13.376294"
+    @type: float
+    @return: location obj containing the location.raw, location.address and location.latitude location.longitude
+             return None if place not found
+    """
+    try:
+        return geolocator.reverse(lat, lon)
     except Exception, e:
         raise PGeoException(e, status_code=400)
