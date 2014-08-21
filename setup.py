93d6ae6457a3759471ce27572c41e95115b5e997
guidofenix <guido.barbaglia@fao.org>
2014-08-21 10:52:09 +0200

    PIP files.

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup
 
 setup(
     name='PGeo',
-    version='0.1.0',
+    version='0.1.1',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=['pgeo'],
-    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
     url='http://pypi.python.org/pypi/PGeo/',
     license='LICENSE.txt',
     description='PGeo module.',
