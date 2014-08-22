1557252517bcc0d3ae73caf26263fa8801cd79df
vortex <simone.murzilli@fao.org>
2014-08-22 14:47:26 +0200

    added process hdfs

--- pgeo/gis/raster.py
+++ pgeo/gis/raster.py
@@ -1,10 +1,11 @@
 from osgeo import gdal, osr
 import os
 import subprocess
+import glob
+import math
 from pgeo.utils import log
 from pgeo.utils import filesystem
 from pgeo.error.custom_exceptions import PGeoException, errors
-import math
 
 log = log.logger(__name__)
 
@@ -233,3 +234,105 @@ def get_authority(file_path):
     prj = ds.GetProjection()
     srs=osr.SpatialReference(wkt=prj)
     return srs.GetAttrValue("AUTHORITY", 0),  srs.GetAttrValue("AUTHORITY", 1)
+
+
+# Process HDFs
+def process_hdfs(obj):
+    log.inf(obj)
+
+    # extract bands
+    hdfs = extract_files_and_band_names(obj["source_path"], obj["band"])
+
+    # extract hdf bands
+    single_hdfs = create_hdf_files(obj["output_path"], hdfs)
+
+    # merge tiles
+    hdf_merged = merge_hdf_files(obj["output_path"], obj["output_path"], obj["gdal_merge"])
+
+    # do stats?
+    #TODO: do stats
+
+    # translate
+    tiff = warp_hdf_file(hdf_merged, obj["output_path"], obj["output_file_name"], obj["gdalwarp"])
+
+    #add overviews
+    if obj.has_key("gdaladdo"):
+        tiff = overviews_tif_file(tiff, obj["gdaladdo"]["parameters"], obj["gdaladdo"]["overviews_levels"])
+
+    return tiff
+
+
+def extract_files_and_band_names(path, band):
+    bands = []
+    hdfs = glob.glob(path + "/*.hdf")
+    for f in hdfs:
+        gtif = gdal.Open(f)
+        sds = gtif.GetSubDatasets()
+        bands.append(sds[int(band) - 1][0])
+    return bands
+
+
+def create_hdf_files(output_path, files):
+    log.info("Create HDF Files")
+    i = 0;
+    for f in files:
+        cmd = "gdal_translate '" + f + "' " + output_path + "/" + str(i) + ".hdf"
+        log.info(cmd)
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        i += 1;
+        #TODO catch the error
+        log.info(output)
+        log.warn(error)
+
+
+def merge_hdf_files(source_path, output_path, parameters=None):
+    log.info("Merge HDF Merge")
+    output_file = output_path + "/output.hdf"
+
+    # creating the cmd
+    cmd = "gdal_merge.py "
+    for key in parameters.keys():
+        cmd += " " + key + " " + str(parameters[key])
+    cmd += " " + source_path + "/*.hdf -o " + output_file
+
+    log.info(cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    log.info(output)
+    log.warn(error)
+    return output_file
+
+
+def warp_hdf_file(source_file, output_path, output_file_name, parameters=None ):
+    log.info("Warp HDF File to Ti")
+    output_file = output_path + "/" + output_file_name
+
+    cmd = "gdalwarp "
+    for key in parameters.keys():
+        cmd += " " + key + " " + str(parameters[key])
+    cmd += " " + source_file + " " + output_file
+
+    log.info(cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    log.info(output)
+    log.warn(error)
+    return output_file
+
+
+def overviews_tif_file(output_file, parameters=None, overviews_levels=None):
+    log.info("Create overviews")
+
+    cmd = "gdaladdo "
+    for key in parameters.keys():
+        cmd += " " + key + " " + str(parameters[key])
+    cmd += " " + output_file
+    cmd += " " + overviews_levels
+
+    log.info(cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    log.info(output)
+    log.warn(error)
+    return output_file
