e7973aff818e7fb29b8a4bebae77daf8d4ce9cb2
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-08-29 14:37:21 +0200

    Standard output for TRMM2.

--- pgeo/dataproviders/trmm2.py
+++ pgeo/dataproviders/trmm2.py
@@ -18,10 +18,14 @@ def list_years():
             ftp.login()
             ftp.cwd(conf['source']['ftp']['data_dir'])
             l = ftp.nlst()
-            l.sort()
+            l.sort(reverse=True)
             out = []
             for s in l:
-                out.append({'code': s, 'label': s})
+                try:
+                    int(s)
+                    out.append({'code': s, 'label': s})
+                except:
+                    pass
             ftp.quit()
             return out
         else:
@@ -99,7 +103,6 @@ def list_layers(year, month, day):
             ftp.cwd(day)
             l = ftp.nlst()
             l.sort()
-            # fao_layers = filter(lambda x: '00.7.1day.' in x, l)
             fao_layers = filter(lambda x: '.tif' in x, l)
             out = []
             for layer in fao_layers:
@@ -126,12 +129,13 @@ def list_layers_subset(year, month, from_day, to_day):
     List all the available layers for a given year and month.
     @param year: e.g. '2010'
     @param month: e.g. '02'
+    @from_day: e.g. 01
+    @to_day: e.g. 05
     @return: An array of code/label/extensions objects.
     """
-
+    file_path_root = 'ftp://' + conf['source']['ftp']['base_url'] + conf['source']['ftp']['data_dir']
     days = map(lambda x: str(x) if x > 9 else '0'+str(x), range(int(from_day), 1+int(to_day)))
     out = []
-
     try:
         if conf['source']['type'] == 'FTP':
             ftp = FTP(conf['source']['ftp']['base_url'])
@@ -140,9 +144,6 @@ def list_layers_subset(year, month, from_day, to_day):
             ftp.cwd(year)
             ftp.cwd(month)
             for i in range(0, len(days)):
-                print i
-                print days[i]
-                print
                 if i > 0:
                     ftp.cwd('../')
                 ftp.cwd(days[i])
@@ -156,7 +157,21 @@ def list_layers_subset(year, month, from_day, to_day):
                     label += ' ('
                     label += '-'.join([year, month, days[i]])
                     label += ', ' + hour + ')'
-                    out.append({'code': code, 'label': label, 'extensions': ['.tif', '.tfw']})
+                    file_path = file_path_root + year + '/' + month + '/' + days[i] + '/' + code
+                    out.append({
+                        'file_name': code,
+                        'file_path': file_path,
+                        'label': label,
+                        'size': None
+                    })
+                    code = code.replace('.tif', '.tfw')
+                    file_path = file_path_root + year + '/' + month + '/' + days[i] + '/' + code
+                    out.append({
+                        'file_name': code,
+                        'file_path': file_path,
+                        'label': label,
+                        'size': None
+                    })
             ftp.quit()
             return out
         else:
