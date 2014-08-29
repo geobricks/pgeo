365f8a9480de02e715b45c518f400891b4bc50d8
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-08-29 11:46:49 +0200

    List a range of layers.

--- pgeo/dataproviders/trmm2.py
+++ pgeo/dataproviders/trmm2.py
@@ -119,3 +119,47 @@ def list_layers(year, month, day):
             raise PGeoException(errors[512], status_code=512)
     except:
         raise PGeoException(errors[511], status_code=511)
+
+
+def list_layers_subset(year, month, from_day, to_day):
+    """
+    List all the available layers for a given year and month.
+    @param year: e.g. '2010'
+    @param month: e.g. '02'
+    @return: An array of code/label/extensions objects.
+    """
+
+    days = map(lambda x: str(x) if x > 9 else '0'+str(x), range(int(from_day), 1+int(to_day)))
+    out = []
+
+    try:
+        if conf['source']['type'] == 'FTP':
+            ftp = FTP(conf['source']['ftp']['base_url'])
+            ftp.login()
+            ftp.cwd(conf['source']['ftp']['data_dir'])
+            ftp.cwd(year)
+            ftp.cwd(month)
+            for i in range(0, len(days)):
+                print i
+                print days[i]
+                print
+                if i > 0:
+                    ftp.cwd('../')
+                ftp.cwd(days[i])
+                l = ftp.nlst()
+                l.sort()
+                fao_layers = filter(lambda x: '.tif' in x, l)
+                for layer in fao_layers:
+                    code = layer
+                    hour = layer[0:layer.index('.tif')].split('.')[2]
+                    label = layer[0:layer.index('.tif')].split('.')[0]
+                    label += ' ('
+                    label += '-'.join([year, month, days[i]])
+                    label += ', ' + hour + ')'
+                    out.append({'code': code, 'label': label, 'extensions': ['.tif', '.tfw']})
+            ftp.quit()
+            return out
+        else:
+            raise PGeoException(errors[512], status_code=512)
+    except:
+        raise PGeoException(errors[511], status_code=511)
