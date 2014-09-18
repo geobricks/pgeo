c0bb7e4a556299542681c50d9535cf1a56473d95
vortex <simone.murzilli@fao.org>
2014-09-18 14:09:25 +0200

    metadata configuration files

---
+++ pgeo/config/metadata/core.py
@@ -0,0 +1,86 @@
+template = {
+    "title" : {
+        "EN" : ""
+    },
+    "parentIdentifiers" : None,
+    "metadataStandardName" : "FENIX",
+    "metadataStandardVersion" : "0.1",
+    "language" : {
+        "codeList" : "Languages",
+        "version" : "1.0",
+        "codes" : [{"code" : "0101"}]
+    },
+    "characterSet" : {
+        "codeList" : "...",
+        "version" : "...",
+        "codes" : [{"code" : "UTF8"}]
+    },
+    "meContent" : {
+        "resourceRepresentationType" : "geographic",
+        "keywords" : [],
+        "description" : {
+            "EN" : ""
+        },
+        "seCoverage" : {
+            "coverageTime" : {},
+            "coverageGeographic" : {},
+            "coverageSector" : {}
+        }
+    },
+    "meStatisticalProcessing" : {
+        "seDatasource" : {
+            "sePrimaryDataCollection" : {
+                "dataCollector" : {
+                    "organisation" : ""
+                },
+                "typeOfCollection" : {
+                    "codeList" : "...",
+                    "version" : "...",
+                    "codes" : [{"code" : ""}]
+                },
+                "collectionPeriodicity" : {
+                    "codeList" : "...",
+                    "version" : "...",
+                    "codes" : [{"code" : ""}]
+                }
+            },
+            "seDataCompilation" : {
+                "aggregationProcessing" : ""
+            }
+        }
+    },
+    "meMaintenance" : {
+        "seUpdate" : {
+        }
+    },
+    "meReferenceSystem" : {
+        "referenceSystemIdentifier": "EPSG",
+        "referenceSystemName": {
+            "EN": "European Petroleum Survey Group"
+        },
+        "referenceSystemAuthority": None,
+        "seProjection" : {
+            "projection" : {
+                "codeList" : "EPSG",
+                "version" : "1.0",
+                "codes" : [{"code" : ""}]
+            },
+            "projectionName" : { "EN" : "..." }
+        },
+        "seDatum" : {
+            "datum" :  {
+                "codeList" : "datum",
+                "version" : "1.0",
+                "codes" : [
+                    {"code" : "WGS84"}
+                ]
+            },
+            "datumName" : {"EN":"WGS84"}
+        }
+    },
+    "meAccessibility" : {
+        "seDistribution": {
+            "onlineResource" : ""
+        }
+    }
+}
---
+++ pgeo/config/metadata/raster.py
@@ -0,0 +1,19 @@
+template = {
+    "meSpatialRepresentation" :{
+        "layerType" : "raster",
+        "seBoundingBox": {
+            "Xmin": None,
+            "Xmax": None,
+            "Ymin": None,
+            "Ymax": None
+        },
+        "seGridSpatialRepresentation" : {
+            "numberOfDimension" : None,
+            "axisDimesionProperties" : None,
+
+            "cellGeometry" : None,
+            "cellOfOrigin" : None,
+            "XYposition" : None
+        }
+    }
+}
